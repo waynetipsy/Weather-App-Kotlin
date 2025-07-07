7dd247787a18abadd4bc1acca64e8c00b3349a4d
waynetipsy <jacketoniru@gmail.com>
2025-07-07 23:28:59 +0100

    Final Screen

--- app/src/main/java/app/ify/weatherapp/MainActivity.kt
+++ app/src/main/java/app/ify/weatherapp/MainActivity.kt
@@ -11,20 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import app.ify.weatherapp.screens.WeatherScreen
 import app.ify.weatherapp.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WeatherScreen()
             }
         }
     }
