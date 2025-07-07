7ebd2c6ab7d93dbbae37f679bfc9f43c517ab397
waynetipsy <jacketoniru@gmail.com>
2025-07-07 12:07:31 +0100

    Screen

--- app/src/main/java/app/ify/weatherapp/screens/WeatherScreen.kt
+++ app/src/main/java/app/ify/weatherapp/screens/WeatherScreen.kt
@@ -1,5 +1,11 @@
 package app.ify.weatherapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -7,7 +13,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import app.ify.weatherapp.viewmodel.WeatherUiState
 import app.ify.weatherapp.viewmodel.WeatherViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,7 +32,36 @@ fun WeatherScreen(
     val selectedLocation by viewModel.selectedLocation.collectAsState()
 
 
-    Scaffold (topBar = { TopAppBar(title = { Text("Weather App with Hilt") }) }){
+    Scaffold (topBar = { TopAppBar(title = { Text("Weather App with Hilt") }) }
+    ){
+        padding ->
+        Column(modifier = Modifier.fillMaxSize()
+            .padding(padding).padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            LocationSelector(
+                locations = locations,
+                selectedLocation = selectedLocation,
+                onLocationSelected = {location ->
+                    viewModel.selectedLocation(location)
+                }
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            // Weather Display based on UI State
+            when(val state = uiState){
+                is WeatherUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                is WeatherUiState.Success -> {
+                    WeatherInfoDisplay(state.weatherInfo)
+                }
+                is WeatherUiState.Error -> {
+                    Text(text = state.errorMessage,
+                     color = Color.Red
+                        )
+                }
+            }
+        }
 
     }
 }
